db7a9540d43c68ce3ef6fe34d8dd0f11af1c196f
phatommobb <nickoluoch1130@gmail.com>
2025-03-27 14:38:57 +0300

    fetch requests

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import Profile from './Profile';
+import FetchGetRequest from './getrequest';
 
 function App() {
   return (
     <div className="App">
-      <Profile delay={1000} />
+      <FetchGetRequest />
     </div>
   );
 }
---
+++ src/getrequest.jsx
@@ -0,0 +1,5 @@
+function FetchGetRequest() {
+  return <div>Hello</div>;
+}
+
+export default FetchGetRequest;
