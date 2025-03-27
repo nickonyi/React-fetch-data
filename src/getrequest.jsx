4ff6fd56419b17e4e885d6b2526fd90ac560b2f3
phatommobb <nickoluoch1130@gmail.com>
2025-03-27 16:13:26 +0300

    react router

--- src/getrequest.jsx
+++ src/getrequest.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { NavLink } from 'react-router-dom';
 
 function FetchGetRequest() {
   const [data, setData] = useState(null);
@@ -9,7 +10,7 @@ function FetchGetRequest() {
     async function fetchDataPosts() {
       try {
         const response = await fetch(
-          `https://jsonplaceholder.typicode.coms/posts?_limit=8`
+          `https://jsonplaceholder.typicode.com/posts?_limit=8`
         );
         if (!response.ok) {
           throw new Error(`HTTP error:status${response.status}`);
@@ -34,6 +35,27 @@ function FetchGetRequest() {
       <div className="w-52 sm:w-80 flex justify-center items-center">
         {loading && <div className="text-lg font-medium">Loading posts...</div>}
         {error && <div className="text-red-700">{error}</div>}
+        <ul>
+          {data &&
+            data.map(({ id, title }) => (
+              <li
+                key={id}
+                className="border-b border-gray-100 text-sm sm:text-base"
+              >
+                <NavLink
+                  className={({ isActive }) => {
+                    const baseClasses = 'p-4 block hover:bg-gray-100';
+                    return isActive
+                      ? `${baseClasses} bg-gray-100`
+                      : baseClasses;
+                  }}
+                  to={`/posts/${id}`}
+                >
+                  {title}
+                </NavLink>
+              </li>
+            ))}
+        </ul>
       </div>
     </div>
   );
