5dd1b6e02b23fb9863069291eca78a3a1f423a1b
phatommobb <nickoluoch1130@gmail.com>
2025-03-27 15:46:03 +0300

    react router

--- src/getrequest.jsx
+++ src/getrequest.jsx
@@ -9,7 +9,7 @@ function FetchGetRequest() {
     async function fetchDataPosts() {
       try {
         const response = await fetch(
-          `https://jsonplaceholder.typicode.com/posts?_limit=8`
+          `https://jsonplaceholder.typicode.coms/posts?_limit=8`
         );
         if (!response.ok) {
           throw new Error(`HTTP error:status${response.status}`);
@@ -26,9 +26,15 @@ function FetchGetRequest() {
     }
     fetchDataPosts();
   }, []);
+
+  console.log(data);
+
   return (
     <div className="flex">
-      <div className="flex"></div>
+      <div className="w-52 sm:w-80 flex justify-center items-center">
+        {loading && <div className="text-lg font-medium">Loading posts...</div>}
+        {error && <div className="text-red-700">{error}</div>}
+      </div>
     </div>
   );
 }
